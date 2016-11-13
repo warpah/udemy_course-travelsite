6941ab7064e88633a8a89bd5e4ec774bfa8f66d5
Chris <clb1985@live.com>
2016-11-12 22:30:57 -0500

    refactored css

--- gulp/tasks/_styles.js
+++ gulp/tasks/_styles.js
@@ -3,11 +3,16 @@ var 	gulp 						= require('gulp'),
 		  autoPrefixer 		= require('autoprefixer'),
 		  cssVars 				= require('postcss-simple-vars'),
 		  nested					= require('postcss-nested'),
-		  cssImport				= require('postcss-import');
+		  cssImport				= require('postcss-import'),
+		  mixins					=	require('postcss-mixins');
 
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postCss([cssImport, cssVars, nested, autoPrefixer]))
+		.pipe(postCss([cssImport, mixins, cssVars, nested, autoPrefixer]))
+		.on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/temp/styles/'));
 });
